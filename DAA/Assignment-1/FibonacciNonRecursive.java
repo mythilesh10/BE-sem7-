2d89303082c63574030f927ad84d3c2a6bef97d0
Mithilesh Kumbhar <94346726+mythilesh10@users.noreply.github.com>
2023-11-06 21:41:54 +0530

    FibonacciNonRecursive2.java

--- DAA/Assignment-1/FibonacciNonRecursive.java
+++ DAA/Assignment-1/FibonacciNonRecursive.java
@@ -21,6 +21,13 @@ public class FibonacciNonRecursive {
         System.out.print("Enter the value of n for the Fibonacci sequence: ");
         int n = scanner.nextInt();
         scanner.close();
+        long startTime = System.nanoTime();
+        long result = fibonacci(n);
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+        long size = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
         System.out.println("Fibonacci of " + n + " is: " + fibonacci(n));
+        System.out.println("Time taken for execution in nanoseconds: " + timeElapsed);
+        System.out.println("Space complexity: " + size + " bytes");
     }
 }
