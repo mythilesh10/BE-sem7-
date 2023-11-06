657144ed446b2c4420f9ca15b8451109c278cd30
Mithilesh Kumbhar <94346726+mythilesh10@users.noreply.github.com>
2023-11-06 21:43:36 +0530

    Update FibonacciRecursiveDynamic.java

--- DAA/Assignment-1/FibonacciRecursiveDynamic.java
+++ DAA/Assignment-1/FibonacciRecursiveDynamic.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 //Time complexity = O(2^n)
 //Space complexity = O(n)
 public class FibonacciRecursiveDynamic {
+    static int calls = 0;
     public static long fibonacci(int n) {
+        calls++;
         if (n <= 1) {
             return n;
         }
@@ -15,6 +17,11 @@ public class FibonacciRecursiveDynamic {
         System.out.print("Enter the value of n for the Fibonacci sequence: ");
         int n = scanner.nextInt();
         scanner.close();
+        long startTime = System.nanoTime();
+        long result = fibonacci(n);
+        long endTime = System.nanoTime();long timeElapsed = endTime - startTime;
+        System.out.println("Time complexity: " + timeElapsed + " nanoseconds");
         System.out.println("Fibonacci of " + n + " is: " + fibonacci(n));
+        System.out.println("Number of function calls: " + calls);
     }
 }
